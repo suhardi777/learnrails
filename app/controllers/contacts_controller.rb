e6f3f93ddf16f1f96febc57f569096e0f6532041
“Suhardi” <suhardi777@yahoo.com>
2014-06-28 14:37:49 +0800

    save data to a spreadsheet

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,40 @@
+class ContactsController < ApplicationController
+#The new rail way
+def new
+	@contact = Contact.new
+end
+def create
+	@contact = Contact.new(secure_params) 
+		if @contact.valid?
+			# TODO save data
+			@contact.update_spreadsheet
+			# TODO send message
+		flash[:notice] = "Message sent from #{@contact.name}." 
+		redirect_to root_path
+		else
+			render :new
+		end 
+	end
+
+private
+def secure_params
+	params.require(:contact).permit(:name, :email, :content)
+end
+
+end
+#the Old pagination way
+ #def process_form
+#if params[:contact][:name].blank?
+#	raise 'Name is blank!' end
+#if params[:contact][:email].blank? 
+#	raise 'Email is blank!'
+#end
+#if params[:contact][:content].blank?
+#	raise 'Message is blank!' end
+#message = "Received request from #{params[:contact][:name]}"
+#redirect_to root_path, :notice => message
+ #Rails.logger.debug "DEBUG: params are #{params}"
+ #flash[:notice] = "Received request from #{params[:contact][:name]}" 
+ #redirect_to root_path
+ #end 
+#end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   #get "/pages/*id", to: 'pages#show'
+  #post 'contact', to: 'contacts#process_form' - the old way
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
