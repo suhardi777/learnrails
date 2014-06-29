fcda711c3c233488e86ec532e85ee13a8e1b5da2
“Suhardi” <suhardi777@yahoo.com>
2014-06-29 14:24:38 +0800

    Sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,6 +9,7 @@ def create
 			# TODO save data
 			@contact.update_spreadsheet
 			# TODO send message
+			UserMailer.contact_email(@contact).deliver
 		flash[:notice] = "Message sent from #{@contact.name}." 
 		redirect_to root_path
 		else
